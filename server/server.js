0c883ee32fa0a2a15b295b8e1843435cc78580a1
Bhchien211 <BuiHongChien@users.noreply.github.com>
2021-06-08 22:04:17 +0300

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,122 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const bodyParser=require('body-parser');
-const { urlencoded } = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
+require("dotenv").config();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const app=express()
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded())
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// MODULES
+const { Blog } = require("./models/Blog");
+const { Quote } = require("./models/Quote");
+
+// Middleware
+const { auth } = require("./middleware/auth");
+
+
+// @route    POST /api/admin/login
+// @desc     admin logins to be allowed manager
+// @access   private
+app.post("/api/admin/login", async (req, res) => {
+  try {
+    const { password } = req.body;
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(process.env.ADMIN_PASSWORD, salt);
+
+    if (!password)
+      res
+        .status(400)
+        .json({ success: false, msg: "Not all fields have been entered" });
+
+    const isMatch = await bcrypt.compare(password, passwordHash);
+    if (!isMatch)
+      res.status(400).json({ success: false, msg: "Invalid password!" });
+
+    const token = jwt.sign(
+      { id: process.env.ADMIN_ID },
+      process.env.JWT_SECRET
+    );
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ success: false, err: "Error!!!" });
+  }
+});
+
+// -------------------------------------------
+//                    Blogs
+// -------------------------------------------
+
+// @route    GET /api/blogs
+// @desc     get all blogs
+// @access   public
+app.get("/api/blogs", async (req, res) => {
+  try {
+    Blog.find().exec((err, doc) => {
+      if (err) return res.status(400).send(err);
+      res.status(200).send(doc);
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server blogs error");
+  }
+});
+
+// @route    POST /api/blogs
+// @desc     post a new blog
+// @access   private
+app.post("/api/admin/blogs", auth, (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    else res.status(200).json({ success: true, doc });
+  });
+});
+//***********************************************
+
+// ----------------------------------------------
+//                    Quotes
+// ----------------------------------------------
+
+// @route    GET /api/quotes
+// @desc     get all quotes
+// @access   public
+app.get("/api/quotes", async (req, res) => {
+  try {
+    Quote.find().exec((err, doc) => {
+      if (err) res.status(400).send(err);
+      res.status(200).send(doc);
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server quotes error");
+  }
+});
+
+// @route    POST /api/quotes
+// @desc     post a new quote
+// @access   private
+app.post("/api/admin/quotes", auth, (req, res) => {
+  const quote = new Quote(req.body);
+
+  quote.save((err, doc) => {
+    if (err) res.json({ success: false, err });
+    else res.status(200).json({ success: true, doc });
+  });
+});
+//***********************************************\
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running at ${port}`));
